cdbad284ad2e957a2e049b4b5292b8091ed84a90
Mikhail <mzhigulin@gmail.com>
2022-10-12 23:22:44 +0700

    BACKWARDED: to Xcode 10
    This commit provides the project compatible with Xcode 10.
    
    BACKWORDED: to Xcode 10.
    
    FIXED: code style in according with swift lint rules.

--- DarkModeDiscovery/DarkModeDiscovery/DemoContent/ViewController.swift
+++ DarkModeDiscovery/DarkModeDiscovery/DemoContent/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
     }
 
     override var representedObject: Any? {
